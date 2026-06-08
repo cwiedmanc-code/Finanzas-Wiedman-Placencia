eb46313827b329242a8064f68e445974f8f63c06
cwiedmanc-code <cwiedmanc@gmail.com>
2026-06-08 11:11:49 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // ── VERSIÓN: cambia este número cada vez que hagas un deploy ──
 // Ej: poritos-v2, poritos-v3, poritos-v4...
-const CACHE = 'poritos-v14';
+const CACHE = 'poritos-v15';
 
 const ASSETS = [
   '/Finanzas-Wiedman-Placencia/',
